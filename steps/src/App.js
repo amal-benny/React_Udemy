2ff73f3a7e27871a7d2c36daf842e17152970992
amal-benny <amalbennyadgjmp@gmail.com>
2025-07-25 15:14:16 +0530

    Learnt how to use useState by creating a steps counter

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,51 +1,54 @@
-import {useState} from "react";
+import { useState } from "react";
+
 const messages = [
-  "Learn ",
-  "Apply",
-  "Invest",
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
 ];
 
-export default function App(){
-  return(
-    <div>
-      <Steps/>
-      <Steps/>
-    </div>
-  );
-}
-
-function Steps(){
-  const [step, setstep] = useState(1);
+export default function App() {
+  const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
-  
-  function handleprevious(){
-    if(step > 1) setstep((s) => s-1);
-  }
 
-  function handleNext(){
-    if (step < 3) setstep((s) => s+1);
+  function handlePrevious() {
+    if (step > 1) setStep((c) => c - 1);
+  }
+  function handleNext() {
+    if (step < 3) setStep((c) => c + 1);
   }
 
   return (
-    <div>
-      <button className="close" onClick={()=> setIsOpen((is)=> !is)}>{isOpen ? "CLOSE" : "OPEN"}</button>
-      { isOpen && (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""} >1</div>
-        <div className={step >= 2 ? "active" : ""} >2</div>
-        <div className={step >= 3 ? "active" : ""} >3</div>
-      </div>
+    <>
+      <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
+        {isOpen ? "X" : "O"}
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <p className="message">Step {step}: {messages[step - 1]} </p>
-
-      <div className="buttons">
-        <button style={{backgroundColor: "#7950f2", color:"#fff"}} onClick={handleprevious} >
-          Previous</button>
-        <button style={{backgroundColor: "#7950f2", color:"#fff"}} onClick={handleNext}>Next</button>
-      </div>
-    </div>)
-}
-    </div>
+          <p className="message">
+            Step {step} : {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
